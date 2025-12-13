7b37b7625cd3f068d5d9db832f6318506bc84a46
Juan David Morales Pulido <jdmoral.ar@gmail.com>
2025-12-13 03:31:01 -0500

    feat: extend android bridge with wireless and compat audit

--- kivy-protonox-version/kivy/protonox_ext/android_bridge/__init__.py
+++ kivy-protonox-version/kivy/protonox_ext/android_bridge/__init__.py
@@ -9,6 +9,7 @@ from .adb import (
     capture_bugreport,
     connect_wireless,
     device_props,
+    enable_wireless,
     ensure_adb,
     install_apk,
     list_devices,
@@ -20,6 +21,8 @@ from .adb import (
     uninstall,
     watch,
 )
+from .bridge_server import BridgeServer
+from .compat import AndroidCompatReport, PermissionStatus, audit_runtime_compat
 from .preflight import AndroidPreflightResult, android_preflight
 
 __all__ = [
@@ -30,6 +33,7 @@ __all__ = [
     "auto_select_device",
     "capture_bugreport",
     "connect_wireless",
+    "enable_wireless",
     "device_props",
     "ensure_adb",
     "install_apk",
@@ -41,6 +45,10 @@ __all__ = [
     "stream_logcat_structured",
     "uninstall",
     "watch",
+    "BridgeServer",
+    "AndroidCompatReport",
+    "PermissionStatus",
+    "audit_runtime_compat",
     "AndroidPreflightResult",
     "android_preflight",
 ]
